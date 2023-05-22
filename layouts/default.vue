2b6ebc7cc943cf0dd58ebc647797aa95e6658d83
Florian <florian.fp.perrier@gmail.com>
2023-05-22 14:45:36 +0200

    fix layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="flex flex-col min-h-screen">
-    <DefaultNavbar>
-      <main class="container mx-auto flex-grow">
-        <slot />
-      </main>
-      <DefaultFooter class="mt-auto" />
-    </defaultnavbar>
+    <DefaultNavbar />
+    <main class="container mx-auto flex-grow">
+      <slot />
+    </main>
+    <DefaultFooter class="mt-auto" />
   </div>
 </template>
